d1eb264a28b436909c151fdbc42c6e35748dd227
Deepanshu Patyal <patyaldeepanshu05@gmail.com>
2025-06-24 03:17:58 +0530

    Update settings.py

--- BharatBuilds/settings.py
+++ BharatBuilds/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -130,7 +129,13 @@ STATICFILES_DIRS=[
  BASE_DIR / "static"
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # where your custom static files are
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTOSLUG_SLUGIFY_FUNCTION = 'django.utils.text.slugify'
 
