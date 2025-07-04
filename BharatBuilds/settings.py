d1c8a2fed83f84aa05015888a2a873ef6e600982
Deepanshu Patyal <patyaldeepanshu05@gmail.com>
2025-07-05 02:27:04 +0530

    add changes

--- BharatBuilds/settings.py
+++ BharatBuilds/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'BharatBuilds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio_db',         # ← jo naam diya database ka
+        'USER': 'postgres',             # ← default username
+        'PASSWORD': 'Patyal@2005',    # ← jo password set kiya installation mein
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
